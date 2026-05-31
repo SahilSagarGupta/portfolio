9b0988f98dac436341cea42738948ab19d38d00a
Sahil Sagar Gupta <sg5346288@gmail.com>
2026-05-31 19:19:05 +0530

    feat: add ThemeContext and Loader splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import Loader from './components/Loader'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Skills from './sections/Skills'
 import Projects from './sections/Projects'
 import Demo from './sections/Demo'
 import Contact from './sections/Contact'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-950 transition-colors duration-300">
       <Loader />
-      <Navbar />
+      {/* <Navbar /> */}
       <main>
-        <Home />
+        <Hero />
         <About />
         <Skills />
         <Projects />
         <Demo />
         <Contact />
       </main>
-      <footer />
+      {/* <Footer /> */}
     </div>
   )
 }
 
-export default App;
+export default App
